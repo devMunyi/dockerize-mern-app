b3dc640c0b9b45a1cf57a8e1801ebc7f34809dcd
devMunyi <samunyi9@gmail.com>
2023-09-17 21:51:57 +0300

    first commit

---
+++ api/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.status(200).json([
+        {
+            "id":"1",
+            "title":"Book Review: The Name of the Wind"
+          },
+          {
+            "id":"2",
+            "title":"Game Review: Pokemon Brillian Diamond"
+          },
+          {
+            "id":"3",
+            "title":"Show Review: Alice in Borderland"
+          }
+    ])
+})
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server ready on port ${port}...`)
+})
